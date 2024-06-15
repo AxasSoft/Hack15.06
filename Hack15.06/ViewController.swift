20c94e34d5a1a74d759eafefc296ac5614a066c0
AxasSoft <44229109+AxasSoft@users.noreply.github.com>
2024-06-15 08:57:00 +0300

    Initial Commit

---
+++ Hack15.06/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hack15.06
+//
+//  Created by Сергей Майбродский on 15.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
